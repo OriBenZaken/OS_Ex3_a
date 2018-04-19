b1d591678892af5fb974a1e97e232e7ab4784a5d
OriBenZaken <orinetanel1@gmail.com>
2018-04-19 11:10:31 +0300

    Done.

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+// Name: Ori Ben Zaken  ID: 311492110
+
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -9,6 +11,13 @@
 #define DIFFERENT 1
 #define BUFFER_SIZE 16
 #define FILE_TERMINATED -2
+
+/**
+ * prints error in sys call to stderr.
+ */
+void printErrorInSysCallToSTDERR() {
+    fprintf(stderr,"Error in system call\n");
+}
 
 /**
  * return the index in buffer of the next non-white-space character in the file.
@@ -45,40 +54,52 @@ int getNextNonWhiteSpaceChar(int file, char buffer[BUFFER_SIZE], int curr_index)
     if (read_bytes == 0) {
         return FILE_TERMINATED;
     }
-    fprintf(stderr,"getNextNonWhiteSpaceChar: Error while reading from file.\n");
+    // Error in read sys call
     printf("getNextNonWhiteSpaceChar: Error while reading from file.\n");
+    printErrorInSysCallToSTDERR();
     return ERROR;
 }
 
-
+/**
+ * comparing between two files.
+ * @param file_path1 path of the first file.
+ * @param file_path2 path of the second file.
+ * @return 1 - files are identical.
+ *         2 - files are similar - differs only in white spaces (space, new line) and in uppercase/lowercase.
+ *         3 - files are different.
+ *         -1 - error occurred in one of the sys calls.
+ */
 int compareFiles(char file_path1[], char file_path2[]) {
-    printf("%s\n%s\n", file_path1, file_path2);
+    printf("First file: %s\nSecond file: %s\n", file_path1, file_path2);
     int result_code = IDENTICAL;
     int file1, file2;
     char buff1[BUFFER_SIZE + 1];
     char buff2[BUFFER_SIZE + 1];
+    // number of bytes which were read from each file.
     int read_bytes_file1, read_bytes_file2;
 
     // open first file
     file1 = open(file_path1, O_RDONLY);
     if (file1 < 0) {
-        printf("Error: failed opening the first file supplied\n");
-        fprintf(stderr,"Error: failed opening the first file supplied\n");
+        printf("Error: failed opening file 1\n");
+        printErrorInSysCallToSTDERR();
         return ERROR;
     }
 
     //open the second file
     file2 = open(file_path2, O_RDONLY);
     if (file2 < 0) {
-        printf("Error: failed opening the second file supplied\n");
-        fprintf(stderr,"Error: failed opening the second file supplied\n");
+        printf("Error: failed opening file 2\n");
+        printErrorInSysCallToSTDERR();
         return ERROR;
     }
 
     int i = 0, j = 0;
     read_bytes_file1 = read(file1, buff1, BUFFER_SIZE);
     read_bytes_file2 = read(file2, buff2, BUFFER_SIZE);
+    // two files are not empty
     if (read_bytes_file1 > 0 && read_bytes_file2 > 0) {
+        // put NULL in the end of the stream. read sys call doesn't do it automatically
         buff1[read_bytes_file1] = '\0';
         buff2[read_bytes_file2] = '\0';
         while (buff1[i] != '\0' && buff2[j] != '\0') {
@@ -86,33 +107,43 @@ int compareFiles(char file_path1[], char file_path2[]) {
             if (buff1[i] == buff2[j]) {
                 i++;
                 j++;
+                // one of the characters from the first file or the second file, or both of them is a white space
             } else if (buff1[i] == '\n' || buff1[i] == ' ' || buff2[j] == '\n' || buff2[j] == ' ') {
                 result_code = SIMILAR;
+                // white space is from the first file. get next non-white-space character
                 if (buff1[i] == '\n' || buff1[i] == ' ') {
                     i = getNextNonWhiteSpaceChar(file1, buff1, i);
+                    // reading from file 1 was over
                     if (i == FILE_TERMINATED) {
                         break;
+                        // error occurred while reading from the file.
                     } else if (i == ERROR) {
                         result_code = ERROR;
                     }
+                    // white space is from the first file. get next non-white-space character
                 } else {
                     j = getNextNonWhiteSpaceChar(file2, buff2, j);
+                    // reading from file 2 was over
                     if (j == FILE_TERMINATED) {
                         break;
                     } else if (j == ERROR) {
+                        // error occurred while reading from the file.
                         result_code = ERROR;
                         break;
                     }
                 }
+                // characters are the same in lowercase, in case of latin letters
             } else if (tolower(buff1[i]) == tolower(buff2[j])) {
                 result_code = SIMILAR;
                 i++;
                 j++;
+                // characters completely different and none of the is a white space
+                // files are different - no need to keep comparing
             } else {
                 result_code = DIFFERENT;
                 break;
             }
-
+            // went through all bytes in buff1
             if (buff1[i] == '\0') {
                 if ((read_bytes_file1 = read(file1, buff1, BUFFER_SIZE)) > 0) {
                     i = 0;
@@ -122,6 +153,7 @@ int compareFiles(char file_path1[], char file_path2[]) {
                 }
             }
 
+            // went through all bytes in buff2
             if (buff2[j] == '\0') {
                 if ((read_bytes_file2 = read(file2, buff2, BUFFER_SIZE)) > 0) {
                     j = 0;
@@ -131,13 +163,15 @@ int compareFiles(char file_path1[], char file_path2[]) {
                 }
             }
         }
+        // at least one of the files is empty or error occurred in the first read sys call
     } else {
+        // one of the file is empty but not both of them
         if ((read_bytes_file1 == 0 || read_bytes_file2 == 0) && !(read_bytes_file1 == 0 && read_bytes_file2 == 0)) {
             result_code = SIMILAR;
         } else if (read_bytes_file1 < 0 || read_bytes_file2 < 0){
-            fprintf(stderr,"Error: failed reading from file\n");
+            printf("Error: failed reading from file\n");
+            printErrorInSysCallToSTDERR();
             result_code = ERROR;
-            printf("Error while reading from files.\n");
         }
     }
 
@@ -153,11 +187,26 @@ int compareFiles(char file_path1[], char file_path2[]) {
         }
     }
 
-    close(file1);
-    close(file2);
+    if (close(file1) == -1) {
+       printf("Error while closing file 1\n");
+       printErrorInSysCallToSTDERR();
+    }
+    if (close(file2) == -1) {
+        printf("Error while closing file 2\n");
+        printErrorInSysCallToSTDERR();
+    }
     return result_code;
 }
 
+/**
+ * comparing two files. recieves files path as command line arguments.
+ * @param argc number of command line arguments including the name of the program.
+ * @param argv command line arguments
+ * @return 1 - files are identical.
+ *         2 - files are similar - differs only in white spaces (space, new line) and in uppercase/lowercase.
+ *         3 - files are different.
+ *         -1 - error occurred in one of the sys calls.
+ */
 int main(int argc, char *argv[]) {
     int result_code;
     if (argc > 3) {
@@ -168,13 +217,13 @@ int main(int argc, char *argv[]) {
         result_code = compareFiles(argv[1], argv[2]);
         switch (result_code) {
             case IDENTICAL:
-                printf("Files are identical.\n");
+                printf("\t-> Files are identical.\n");
                 break;
             case SIMILAR:
-                printf("Files are similar.\n");
+                printf("\t-> Files are similar.\n");
                 break;
             case DIFFERENT:
-                printf("Files are different.\n");
+                printf("\t-> Files are different.\n");
                 break;
             default:
                 break;
